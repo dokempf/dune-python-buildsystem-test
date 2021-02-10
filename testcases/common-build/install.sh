a8353ccf88243e5bb74e5a797b2af07f7116dff3
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-02-10 17:15:40 +0100

    More fixup

--- testcases/common-build/install.sh
+++ testcases/common-build/install.sh
@@ -12,6 +12,6 @@ DUNECONTROL_OPTS="--builddir=$(pwd)/build/common-build --opts=./testcases/common
 # Testing the Python code
 
 ## FIXME: The following line is currently necessary, but should vanish
-./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make install_python
+sudo ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make install_python
 
 ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make test_python
