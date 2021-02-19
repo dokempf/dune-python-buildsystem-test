5005a916b9ab31d23cba448704e0f506243c4796
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-02-19 09:39:15 +0100

    Enable shared library test case

--- testcases/grid-shared-libs/install.sh
+++ testcases/grid-shared-libs/install.sh
@@ -11,3 +11,10 @@ DUNECONTROL_OPTS="--builddir=$(pwd)/build/grid-shared-libs --opts=./testcases/gr
 
 # Testing the Python code
 ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make test_python
+
+# Installing and removing the build tree
+sudo ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make install
+rm -rf build
+
+# Testing the globally installed Python package
+python -c "import dune.grid"
