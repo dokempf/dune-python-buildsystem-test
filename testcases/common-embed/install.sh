9ff935705602fe978bd0e3022db3dbda5a7c48ad
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-02-19 17:45:58 +0100

    Enable the embedding test

--- testcases/common-embed/install.sh
+++ testcases/common-embed/install.sh
@@ -9,15 +9,14 @@ set -e
 DUNECONTROL_OPTS="--builddir=$(pwd)/build/common-embed --opts=./testcases/common-embed/config.opts --module=dune-common"
 ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS all
 
-# Build C++ tests related with Python (build_tests would be too expensive here)
+# Build C++ tests related with embedded Python
 ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make test_embed1
 ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make test_embed2
 
-./dune-common/bin/dunecontrol $DUNE_CONTROL_OPTS bexec ctest -R embed
+pushd build/common-embed/dune-common/dune/python/test
+./test_embed1
+./test_embed2
 
-# Installing and removing the build tree
-sudo ./dune-common/bin/dunecontrol $DUNECONTROL_OPTS make install
-rm -rf builds
-
-# Testing that the installed embedded Python application works
-test_embed2
+# Here, we should also test installing and running the installed executable
+# I have done this, but I do not want to commit installing a test in Dune common.
+# Might add a dune-fufem case to cover that.
